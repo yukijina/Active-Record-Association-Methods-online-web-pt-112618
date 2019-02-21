cabeb32ba68d25a181e4e715037ac82c8414231a
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-21 14:07:40 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -10,7 +10,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.songs.size
+    self.songs.count
   end
 
   def genre_count
