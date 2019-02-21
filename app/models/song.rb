3a2db0d23e1c5d9dae4fb103ed73723903356bca
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-21 14:32:48 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,5 +9,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    self.artist = Artist.create(name: "Drake")
   end
 end
