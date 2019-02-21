19e0637aeb0726d0884fa0c8de0e4618c8087dba
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-21 14:27:18 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
     self.artists.map {|artist| artist.name}
   end
 end
